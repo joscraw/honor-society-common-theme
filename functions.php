3b047a11b6e994522451cab2eb1d6417b80e8756
Josh Crawmer <joshcrawmer4@yahoo.com>
2018-08-10 12:08:26 -0500

    initial commit

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+
+/**
+ * Enable ACF 5 early access
+ * Requires at least version ACF 4.4.12 to work
+ */
+define('ACF_EARLY_ACCESS', '5');
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+// silence is golden
+
+global $crm_single_sign_on_link;
+
+echo $crm_single_sign_on_link("https://www.giftedhire.com/home", "Gifted Hire");
