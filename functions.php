72e6728f559844a6f0a9126bb18607d55842eab8
Josh Crawmer <joshcrawmer4@yahoo.com>
2018-08-19 21:20:02 -0500

    at a good stopping point

--- functions.php
+++ functions.php
@@ -27,3 +27,38 @@ add_filter('tribe_events_pro_widget_calendar_stylesheet_url', function() {
     $styleUrl = get_template_directory_uri() . '/assets/css/custom-events-pro-widget-stylesheet.css';
     return $styleUrl;
 });
+
+/*add_filter( 'tribe_events_pre_get_posts', 'redirect_from_events' );
+
+function redirect_from_events( $query ) {
+
+    if ( is_user_logged_in() )
+        return;
+
+    if ( ! $query->is_main_query() || ! $query->get( 'eventDisplay' ) )
+        return;
+
+// Look for a page with a slug of "logged-in-users-only".
+    $target_page = get_posts( array(
+        'post_type' => 'page',
+        'name' => 'logged-in-users-only'
+    ) );
+
+// Use the target page URL if found, else use the home page URL.
+    if ( empty( $target_page ) ) {
+        $url = get_home_url();
+    } else {
+        $target_page = current( $target_page );
+        $url = get_permalink( $target_page->ID );
+    }
+
+// Redirect!
+    wp_safe_redirect( $url );
+    exit;
+}*/
+
+function my_custom_login()
+{
+    echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('stylesheet_directory') . '/assets/css/login.css" />';
+}
+add_action('login_head', 'my_custom_login');
---
+++ page-login.php
@@ -0,0 +1,4 @@
+<?php get_template_part( 'content', 'login' ); ?>
+
+
+<h1>Login Page</h1>
