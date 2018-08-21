1ba71204770d61ae12ccf8e0f7638613ab25cca3
Josh Crawmer <joshcrawmer4@yahoo.com>
2018-08-20 19:54:08 -0500

    built login page

--- functions.php
+++ functions.php
@@ -57,8 +57,48 @@ function redirect_from_events( $query ) {
     exit;
 }*/
 
-function my_custom_login()
+
+/* Main redirection of the default login page */
+function redirect_login_page() {
+    $login_page  = home_url('/login/');
+    $page_viewed = basename($_SERVER['REQUEST_URI']);
+
+    if($page_viewed == "wp-login.php" && $_SERVER['REQUEST_METHOD'] == 'GET') {
+        wp_redirect($login_page);
+        exit;
+    }
+}
+add_action('init','redirect_login_page');
+
+/* Where to go if a login failed */
+function custom_login_failed() {
+    $login_page  = home_url('/login/');
+    wp_redirect($login_page . '?login=failed');
+    exit;
+}
+add_action('wp_login_failed', 'custom_login_failed');
+
+/* Where to go if any of the fields were empty */
+function verify_user_pass($user, $username, $password) {
+    $login_page  = home_url('/login/');
+    if($username == "" || $password == "") {
+        wp_redirect($login_page . "?login=empty");
+        exit;
+    }
+}
+add_filter('authenticate', 'verify_user_pass', 1, 3);
+
+/* What to do on logout */
+function logout_redirect() {
+    $login_page  = home_url('/login/');
+    wp_redirect($login_page . "?login=false");
+    exit;
+}
+add_action('wp_logout','logout_redirect');
+
+
+/*function my_custom_login()
 {
     echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('stylesheet_directory') . '/assets/css/login.css" />';
 }
-add_action('login_head', 'my_custom_login');
+add_action('login_head', 'my_custom_login');*/
