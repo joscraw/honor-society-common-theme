751d72fc9b1831c04ac2c52cbe8bd8dd8db9bd15
Josh Crawmer <joshcrawmer4@yahoo.com>
2018-11-04 15:34:34 -0600

    added more responsive and mobile friendly pages. Also built in dropdown menu

--- header.php
+++ header.php
@@ -28,6 +28,6 @@
 <body <?php body_class(); ?>>
 
     <header class="main-header">
-        <div><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri() . '/assets/images/main_logo.png'; ?>"></a></div>
+        <div class="main-logo"><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri() . '/assets/images/main_logo.png'; ?>"></a></div>
         <?php get_template_part( 'inc/main', 'nav' ); ?>
     </header>
--- inc/main-nav.php
+++ inc/main-nav.php
@@ -2,7 +2,23 @@
     <?php wp_nav_menu( array(
         'theme_location' => 'primary-menu'
     ) ); ?>
-    <?php wp_nav_menu( array(
+    <?php /*wp_nav_menu( array(
         'menu' => 'right-primary-menu'
-    ) ); ?>
+    ) ); */?>
+
+    <div class="hamburger-menu-icon"><a href="javascript:void(0)"><img width="30" height="30" src="<?php echo get_template_directory_uri() . '/assets/images/menu-icon.png'; ?>"></a></div>
 </nav>
+
+
+
+<script type="text/javascript">
+    jQuery(document).ready(function($) {
+
+        $(".hamburger-menu-icon").click(function(){
+            console.log("hi");
+             $(".menu-primary-menu-container").toggleClass('menu-active');
+            $(".main-content").toggleClass('menu-active');
+        });
+
+    });
+</script>
